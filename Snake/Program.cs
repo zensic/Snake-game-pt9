6ae07034ec62d43da2821af21d42a86c3528ef61
zensic <anthonyleehj@gmail.com>
2020-04-22 14:48:13 +0800

    Implemented Borland-Inspired Coding Style

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -12,10 +12,10 @@ namespace Snake
     {
         public int row;
         public int col;
-        public Position(int row, int col)
+        public Position(int aRow, int aCol)
         {
-            this.row = row;
-            this.col = col;
+            row = aRow;
+            col = aCol;
         }
     }
 
@@ -28,12 +28,12 @@ namespace Snake
             byte left = 1;
             byte down = 2;
             byte up = 3;
-            int lastFoodTime = 0;
-            int foodDissapearTime = 8000; // time for food to dissapear in milliseconds
-            int negativePoints = 0; //
+            int gLastFoodTime = 0;
+            int gFoodDissapearTime = 8000; // time for food to dissapear in milliseconds
+            int gNegativePoints = 0; //
 
             // Snake Default value
-            Position[] directions = new Position[]
+            Position[] gDirections = new Position[]
             {
                 new Position(0, 1), // right
                 new Position(0, -1), // left
@@ -41,14 +41,14 @@ namespace Snake
                 new Position(-1, 0), // up
             };
 
-            double sleepTime = 100; // Velocity of snake
-            int direction = right; // Initialize default snake direction
-            Random randomNumbersGenerator = new Random(); // Random number generator
+            double gSleepTime = 100; // Velocity of snake
+            int gDirection = right; // Initialize default snake direction
+            Random gRandomNumbersGenerator = new Random(); // Random number generator
             Console.BufferHeight = Console.WindowHeight; // Intialize the height of the game window
-            lastFoodTime = Environment.TickCount; // Get time since program has started
+            gLastFoodTime = Environment.TickCount; // Get time since program has started
 
             // Initialize obstacle locations
-            List<Position> obstacles = new List<Position>()
+            List<Position> gObstacles = new List<Position>()
             {
                 new Position(12, 12),
                 new Position(14, 20),
@@ -58,7 +58,7 @@ namespace Snake
             };
 
             // Initialize obstacle color
-            foreach (Position obstacle in obstacles)
+            foreach (Position obstacle in gObstacles)
             {
                 Console.ForegroundColor = ConsoleColor.Cyan;
                 Console.SetCursorPosition(obstacle.col, obstacle.row);
@@ -66,26 +66,26 @@ namespace Snake
             }
 
             // Initialize length of snake
-            Queue<Position> snakeElements = new Queue<Position>();
+            Queue<Position> gSnakeElements = new Queue<Position>();
             for (int i = 0; i <= 5; i++)
             {
-                snakeElements.Enqueue(new Position(0, i));
+                gSnakeElements.Enqueue(new Position(0, i));
             }
 
             // Initialize food
-            Position food;
+            Position gFood;
             do
             {
-                food = new Position(randomNumbersGenerator.Next(0, Console.WindowHeight),
-                    randomNumbersGenerator.Next(0, Console.WindowWidth)); // Initialize coordinate of food (random)
+                gFood = new Position(gRandomNumbersGenerator.Next(0, Console.WindowHeight),
+                    gRandomNumbersGenerator.Next(0, Console.WindowWidth)); // Initialize coordinate of food (random)
             }
-            while (snakeElements.Contains(food) || obstacles.Contains(food)); // To detect whether the food collides with the obstacles/snake body 
-            Console.SetCursorPosition(food.col, food.row);
+            while (gSnakeElements.Contains(gFood) || gObstacles.Contains(gFood)); // To detect whether the food collides with the obstacles/snake body 
+            Console.SetCursorPosition(gFood.col, gFood.row);
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.Write("@");
 
             // Initialize snake body
-            foreach (Position position in snakeElements)
+            foreach (Position position in gSnakeElements)
             {
                 Console.SetCursorPosition(position.col, position.row);
                 Console.ForegroundColor = ConsoleColor.DarkGray;
@@ -95,7 +95,7 @@ namespace Snake
             // Main game loop
             while (true)
             {
-                negativePoints++; // This variable will be used to deduct from the total score
+                gNegativePoints++; // This variable will be used to deduct from the total score
 
                 if (Console.KeyAvailable)
                 {
@@ -104,41 +104,41 @@ namespace Snake
                     // Prevents snake from going backwards
                     if (userInput.Key == ConsoleKey.LeftArrow)
                     {
-                        if (direction != right) direction = left;
+                        if (gDirection != right) gDirection = left;
                     }
                     if (userInput.Key == ConsoleKey.RightArrow)
                     {
-                        if (direction != left) direction = right;
+                        if (gDirection != left) gDirection = right;
                     }
                     if (userInput.Key == ConsoleKey.UpArrow)
                     {
-                        if (direction != down) direction = up;
+                        if (gDirection != down) gDirection = up;
                     }
                     if (userInput.Key == ConsoleKey.DownArrow)
                     {
-                        if (direction != up) direction = down;
+                        if (gDirection != up) gDirection = down;
                     }
                 }
 
-                Position snakeHead = snakeElements.Last(); // Returns last element in the queue, assigns the element as coordinate of snakeHead
-                Position nextDirection = directions[direction]; // The direction will be converted to integer as index, nextDirection will store the difference of the next coordinate
+                Position gSnakeHead = gSnakeElements.Last(); // Returns last element in the queue, assigns the element as coordinate of snakeHead
+                Position gNextDirection = gDirections[gDirection]; // The direction will be converted to integer as index, nextDirection will store the difference of the next coordinate
 
-                Position snakeNewHead = new Position(snakeHead.row + nextDirection.row,
-                    snakeHead.col + nextDirection.col); //
+                Position gSnakeNewHead = new Position(gSnakeHead.row + gNextDirection.row,
+                    gSnakeHead.col + gNextDirection.col); //
 
                 // Makes sure that the snake doesn't go out of bounds
-                if (snakeNewHead.col < 0) snakeNewHead.col = Console.WindowWidth - 1;
-                if (snakeNewHead.row < 0) snakeNewHead.row = Console.WindowHeight - 1;
-                if (snakeNewHead.row >= Console.WindowHeight) snakeNewHead.row = 0;
-                if (snakeNewHead.col >= Console.WindowWidth) snakeNewHead.col = 0;
+                if (gSnakeNewHead.col < 0) gSnakeNewHead.col = Console.WindowWidth - 1;
+                if (gSnakeNewHead.row < 0) gSnakeNewHead.row = Console.WindowHeight - 1;
+                if (gSnakeNewHead.row >= Console.WindowHeight) gSnakeNewHead.row = 0;
+                if (gSnakeNewHead.col >= Console.WindowWidth) gSnakeNewHead.col = 0;
 
                 // If the snake head collides with the snake body or the snake head hits an obstacle, the game loop ends
-                if (snakeElements.Contains(snakeNewHead) || obstacles.Contains(snakeNewHead))
+                if (gSnakeElements.Contains(gSnakeNewHead) || gObstacles.Contains(gSnakeNewHead))
                 {
                     Console.SetCursorPosition(0, 0);
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Game over!");
-                    int userPoints = (snakeElements.Count - 6) * 100 - negativePoints;
+                    int userPoints = (gSnakeElements.Count - 6) * 100 - gNegativePoints;
                     if (userPoints < 0) userPoints = 0;
                     userPoints = Math.Max(userPoints, 0);
                     Console.WriteLine("Your points are: {0}", userPoints);
@@ -146,46 +146,46 @@ namespace Snake
                     return;
                 }
 
-                Console.SetCursorPosition(snakeHead.col, snakeHead.row);
+                Console.SetCursorPosition(gSnakeHead.col, gSnakeHead.row);
                 Console.ForegroundColor = ConsoleColor.DarkGray;
                 Console.Write("*");
 
-                snakeElements.Enqueue(snakeNewHead);
-                Console.SetCursorPosition(snakeNewHead.col, snakeNewHead.row);
+                gSnakeElements.Enqueue(gSnakeNewHead);
+                Console.SetCursorPosition(gSnakeNewHead.col, gSnakeNewHead.row);
                 Console.ForegroundColor = ConsoleColor.Gray;
-                if (direction == right) Console.Write(">");
-                if (direction == left) Console.Write("<");
-                if (direction == up) Console.Write("^");
-                if (direction == down) Console.Write("v");
+                if (gDirection == right) Console.Write(">");
+                if (gDirection == left) Console.Write("<");
+                if (gDirection == up) Console.Write("^");
+                if (gDirection == down) Console.Write("v");
 
 
-                if (snakeNewHead.col == food.col && snakeNewHead.row == food.row)
+                if (gSnakeNewHead.col == gFood.col && gSnakeNewHead.row == gFood.row)
                 {
                     // feeding the snake
                     do
                     {
-                        food = new Position(randomNumbersGenerator.Next(0, Console.WindowHeight),
-                            randomNumbersGenerator.Next(0, Console.WindowWidth)); // Assign two random values into food position
+                        gFood = new Position(gRandomNumbersGenerator.Next(0, Console.WindowHeight),
+                            gRandomNumbersGenerator.Next(0, Console.WindowWidth)); // Assign two random values into food position
                     }
-                    while (snakeElements.Contains(food) || obstacles.Contains(food)); // To detect whether the snake/obstacle collides with food
+                    while (gSnakeElements.Contains(gFood) || gObstacles.Contains(gFood)); // To detect whether the snake/obstacle collides with food
 
-                    lastFoodTime = Environment.TickCount; // 
-                    Console.SetCursorPosition(food.col, food.row);
+                    gLastFoodTime = Environment.TickCount; // 
+                    Console.SetCursorPosition(gFood.col, gFood.row);
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.Write("@");
-                    sleepTime--; // Increase the velocity that the snake is travelling
+                    gSleepTime--; // Increase the velocity that the snake is travelling
 
                     Position obstacle = new Position(); // Initialize position of obstacle
 
                     do
                     {
-                        obstacle = new Position(randomNumbersGenerator.Next(0, Console.WindowHeight),
-                            randomNumbersGenerator.Next(0, Console.WindowWidth)); // Assign to random values into obstacle position
+                        obstacle = new Position(gRandomNumbersGenerator.Next(0, Console.WindowHeight),
+                            gRandomNumbersGenerator.Next(0, Console.WindowWidth)); // Assign to random values into obstacle position
                     }
-                    while (snakeElements.Contains(obstacle) ||
-                        obstacles.Contains(obstacle) ||
-                        (food.row != obstacle.row && food.col != obstacle.row)); // Makes sure the obstacles do not spawn inside the food or other obstacles
-                    obstacles.Add(obstacle); // Adds a new obstacle to the queue
+                    while (gSnakeElements.Contains(obstacle) ||
+                        gObstacles.Contains(obstacle) ||
+                        (gFood.row != obstacle.row && gFood.col != obstacle.row)); // Makes sure the obstacles do not spawn inside the food or other obstacles
+                    gObstacles.Add(obstacle); // Adds a new obstacle to the queue
                     Console.SetCursorPosition(obstacle.col, obstacle.row); // Moves cursor to the obstacle about to drawn
                     Console.ForegroundColor = ConsoleColor.Cyan; // Join
                     Console.Write("="); // 
@@ -193,32 +193,32 @@ namespace Snake
                 else
                 {
                     // moving...
-                    Position last = snakeElements.Dequeue(); // Deletes the last element of the list
+                    Position last = gSnakeElements.Dequeue(); // Deletes the last element of the list
                     Console.SetCursorPosition(last.col, last.row); // Moves cursor to the position of deleted element
                     Console.Write(" "); // Replaces the space in the position with space
                 }
 
-                if (Environment.TickCount - lastFoodTime >= foodDissapearTime) // Food destructor
+                if (Environment.TickCount - gLastFoodTime >= gFoodDissapearTime) // Food destructor
                 {
-                    negativePoints = negativePoints + 50; // Deduct points for not eating food
-                    Console.SetCursorPosition(food.col, food.row); // Moves cursor to the food about to be deleted
+                    gNegativePoints = gNegativePoints + 50; // Deduct points for not eating food
+                    Console.SetCursorPosition(gFood.col, gFood.row); // Moves cursor to the food about to be deleted
                     Console.Write(" "); // Deletes the food drawn at that position
                     do
                     {
-                        food = new Position(randomNumbersGenerator.Next(0, Console.WindowHeight),
-                            randomNumbersGenerator.Next(0, Console.WindowWidth)); // Assigns new position to the food
+                        gFood = new Position(gRandomNumbersGenerator.Next(0, Console.WindowHeight),
+                            gRandomNumbersGenerator.Next(0, Console.WindowWidth)); // Assigns new position to the food
                     }
-                    while (snakeElements.Contains(food) || obstacles.Contains(food)); // Loops for a new position for food until a valid position is set and drawn
-                    lastFoodTime = Environment.TickCount; // 
+                    while (gSnakeElements.Contains(gFood) || gObstacles.Contains(gFood)); // Loops for a new position for food until a valid position is set and drawn
+                    gLastFoodTime = Environment.TickCount; // 
                 }
 
-                Console.SetCursorPosition(food.col, food.row); // Moves cursor to the food about to be deleted
+                Console.SetCursorPosition(gFood.col, gFood.row); // Moves cursor to the food about to be deleted
                 Console.ForegroundColor = ConsoleColor.Yellow; // Set the food color to yellow
                 Console.Write("@"); // Draw the @ on the food position
 
-                sleepTime -= 0.01; // Increase the velocity of the snake each time the loop is run
+                gSleepTime -= 0.01; // Increase the velocity of the snake each time the loop is run
 
-                Thread.Sleep((int)sleepTime); // 
+                Thread.Sleep((int)gSleepTime); // 
             }
         }
     }
